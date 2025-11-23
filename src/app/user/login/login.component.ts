adfe350dc5d37a52f405ded0b16800a9a0bfcde8
dush <dushan1855@gmail.com>
2025-11-23 18:52:53 +0530

    Add logo and modified CSS

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -8,9 +8,23 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   constructor(public formBuilder: FormBuilder) {}
+  isSubmitted: boolean = false;
 
   form = this.formBuilder.group({
     email: ['', [Validators.required]],
     password: ['', [Validators.required]],
   });
+
+  // Method to check if a control has displayable error
+  hasDisplayableError(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return (
+      Boolean(control?.invalid) &&
+      (this.isSubmitted || Boolean(control?.touched))
+    );
+  }
+  onSubmit() {
+    this.isSubmitted = true;
+    console.log('Form Submitted', this.form.value);
+  }
 }
