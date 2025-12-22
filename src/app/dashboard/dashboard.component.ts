7008fcaba94c48d18b28b6f3de21c530aec917c3
dush <dushan1855@gmail.com>
2025-12-22 11:48:19 +0530

    Dash Board

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,10 +4,23 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [],
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
   constructor(private router: Router) {}
+
+  menuValue: boolean = false;
+  menu_icon: string = 'bi bi-list';
+
+  openMenu() {
+    this.menuValue = !this.menuValue;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+
+  closeMenu() {
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
 
   onLogout() {
     localStorage.removeItem('token');
